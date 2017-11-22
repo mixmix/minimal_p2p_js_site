c5d58e752c8b077620f1cae6b2334f86d10fcb6b
mix irving <whimful@gmail.com>
2017-11-22 17:18:00 +1300

    add sheet-router and mutiple pages

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # minimal p2p js site
 
 
-# v1 - hello world
+## v1 - hello world
 
 Run this to take your `index.js` and convert all the fancy node scripts into a browser friendly `bundle.js`.
 
@@ -17,4 +17,14 @@ Note the `.gitignore` and `.datignore` files - these tell git and DAT what to ig
 New deps:
 - [yo-yo](https://github.com/maxogden/yo-yo): a sick and ultra-light templating + dom-manipulation tool
 - [browserify](https://github.com/browserify/browserify): makes node code browser friendly
+
+
+## v2 - basic routing
+
+Add a mutiple pages and a router to jump us between those pages.
+
+Remember that you need to run `npm run build` to compile a new `bundle.js` any time your change your source files.
+
+New deps:
+- [sheet-router](https://github.com/yoshuawuyts/sheet-router): simple router with nice click handler 
 
--- index.js
+++ index.js
@@ -1,9 +1,66 @@
-var yo = require('yo-yo')
+const yo = require('yo-yo')
+const sheetRouter = require('sheet-router')
+const href = require('sheet-router/href')
 
-var app = document.getElementById('app')
-
-var homePage = yo`
-<h1>Butts</h1>
+const homePage = () => yo `
+<div> 
+  <nav>
+    <a href='/'>Home</a>
+    <a href='/projects'>Projects</a>
+    <a href='/about'>About</a>
+  </nav>
+  <h1>Home</h1>
+  <div>
+    Welcome
+  </div>
+</div>
 `
 
-yo.update(app, homePage)
+const projectsPage = (params) => yo `
+<div> 
+  <nav>
+    <a href='/'>Home</a>
+    <a href='/projects'>Projects</a>
+    <a href='/about'>About</a>
+  </nav>
+  <h1>Projects</h1>
+  <div>
+    check out these sweet projects
+  </div>
+</div>
+`
+
+const aboutPage = (params) => yo `
+<div> 
+  <nav>
+    <a href='/'>Home</a>
+    <a href='/projects'>Projects</a>
+    <a href='/about'>About</a>
+  </nav>
+  <h1>About</h1>
+  <div>
+    my passions are ...
+    my favourite search engine is <a href='https://duckduckgo.com'>duckduckgo</a>
+  </div>
+</div>
+`
+
+const router = sheetRouter({ default: '/' }, [
+  ['/', homePage],
+  ['/projects', projectsPage],
+  ['/about', aboutPage],
+])
+
+var app = document.getElementById('app')   // root node our app will be mounted at
+
+function render (path) {
+  const newView = router(path)  // renders the route that matches 'path'
+  yo.update(app, newView)   // morphs 'app' to be that newView 
+}
+
+// Render the home page
+render('/')
+
+// Install click handler for all site anchors!
+href(link => render(link.pathname))
+
